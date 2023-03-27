7c51b4f261be21ff24957c5867f7560416fd716e
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-03-27 14:25:15 +0200

    Tree Validations

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,4 +1,11 @@
 class Tree < ApplicationRecord
   belongs_to :user
   has_many :adoptions, dependent: :destroy
+
+  FRUIT_TYPES = ["Apple", "Apricot", "Cherry", "Chestnut", "Coconut tree", "Fig", "Grapefruit", "Hazel", "Mandarin", "Mango", "Mirabellier", "Mulberry", "Olivier", "Orange", "Pear", "Plum"]
+  validates :name, :address, :fruit_type, :description, :price_per_year, :quantity_per_year, presence: true
+  validates :fruit_type, inclusion: {in: FRUIT_TYPES}
+  validates :description, length: { in: 20..280 }
+  validates :quantity_per_year, numericality: { greater_than: 0 }
+  validates :price, numericality: { greater_than: 0 }
 end

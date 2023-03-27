b2f50569b4278725ee58fa8b0a01c189d19a788f
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-03-27 15:39:04 +0200

    Completed Index / Create / New

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -3,4 +3,28 @@ class TreesController < ApplicationController
     def index
         @trees = Tree.all
     end
+
+    def new
+        @tree = Tree.new
+    end
+
+    def create
+        @tree = Tree.new(tree_params)
+        @tree.user = current_user
+        if @tree.save
+            redirect_to tree_path(@tree)
+        else
+            render :status, :unprocessable_entity
+        end
+    end
+
+    def show
+        @tree = Tree.find(params[:id])
+    end
+
+    private
+
+    def tree_params
+        params.require(:tree).permit(:name, :address, :fruit_type, :description, :price_per_year, :quantity_per_year)
+    end
 end

4e1b887198af5fe9daefeb0172606139e8ffc0bf
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-03-27 14:42:09 +0200

    finished validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :adoption, dependent: :destroy
   belongs_to :user
+
+  validates :rating, :content, presence: true
+  validates :rating, :numericality{ only_integer: true, in: 0..5 }
+  validates :content, :length{ less_than: 3, greater_than:114 }
 end

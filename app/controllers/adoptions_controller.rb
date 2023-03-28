e1c65fc4ede7234310c194ab5ad0db810fe8ba69
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-03-28 12:20:10 +0200

    Added the Adoption instances

--- app/controllers/adoptions_controller.rb
+++ app/controllers/adoptions_controller.rb
@@ -1,2 +1,26 @@
 class AdoptionsController < ApplicationController
+
+    def new
+        @tree = Tree.find(params[:id])
+        @adoption = Adoption.new(starts_at: Date.today, ends_at: Date.today + 1.year)
+    end
+
+    def create
+        @tree = Tree.find(params[:id])
+        @adoption = Adoption.new(adoption_params)
+        @adoption.user = current_user
+        @adoption.tree = @tree
+        if @adoption.save
+            redirect_to tree_path(@tree)
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+
+    private 
+
+    def adoption_params
+        parmas.require(:adoption).require(:name, :starts_at, :ends_at)
+    end
 end

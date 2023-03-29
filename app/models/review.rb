59e58544a840c781c28bef4d8e2c5c0de5402162
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-03-29 14:57:16 +0200

    Some changes to push Heroku

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :adoption, dependent: :destroy
   belongs_to :user
 
-  validates :rating, :content, presence: true
-  validates :rating, :numericality{ only_integer: true, in: 0..5 }
-  validates :content, :length{ less_than: 3, greater_than:114 }
+  # validates :rating, :content, presence: true
+  # validates :rating, :numericality{ only_integer: true, in: 0..5 }
+  # validates :content, :length{ less_than: 3, greater_than:114 }
 end
--- app/models/tree.rb
+++ app/models/tree.rb
@@ -7,10 +7,10 @@ class Tree < ApplicationRecord
   has_one_attached :photo
 
 
-  FRUIT_TYPES = ["Apple", "Apricot", "Cherry", "Chestnut", "Coconut tree", "Fig", "Grapefruit", "Hazel", "Mandarin", "Mango", "Mirabellier", "Mulberry", "Olivier", "Orange", "Pear", "Plum"]
-  validates :name, :address, :fruit_type, :description, :price_per_year, :quantity_per_year, presence: true
-  validates :fruit_type, inclusion: {in: FRUIT_TYPES}
-  validates :description, length: { in: 20..280 }
-  validates :quantity_per_year, numericality: { greater_than: 0 }
-  validates :price_per_year, numericality: { greater_than: 0 }
+  # FRUIT_TYPES = ["Apple", "Apricot", "Cherry", "Chestnut", "Coconut tree", "Fig", "Grapefruit", "Hazel", "Mandarin", "Mango", "Mirabellier", "Mulberry", "Olivier", "Orange", "Pear", "Plum"]
+  # validates :name, :address, :fruit_type, :description, :price_per_year, :quantity_per_year, presence: true
+  # validates :fruit_type, inclusion: {in: FRUIT_TYPES}
+  # validates :description, length: { in: 20..280 }
+  # validates :quantity_per_year, numericality: { greater_than: 0 }
+  # validates :price_per_year, numericality: { greater_than: 0 }
 end

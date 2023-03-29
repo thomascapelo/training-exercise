e716fda6a298c08b41ea14523e3bb6f1d12da1b3
Thomas Cappelli <thomas.cappelli@gmail.com>
2023-03-29 14:46:59 +0200

    changed the root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: 'trees#index'
   get "/instructions", to: "pages#instructions", as: "instructions"
 
   resources :trees do 
-    resources :adoptions, only: [:new, :create]
+    resources :adoptions, only: [:new, :creates]
   end
   resources :adoptions, only: [:index]
 end
